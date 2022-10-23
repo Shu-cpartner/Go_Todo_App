875dd37dabea9af02a1b33089d2c299ca269c85a
Shuji <zaha@cprtnr.com>
2022-10-23 22:19:47 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module golang_basic/Go_Todo_App
+
+go 1.18
+
+require gopkg.in/go-ini/ini.v1 v1.67.0
+
+require github.com/stretchr/testify v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"golang_basic/Go_Todo_App/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
