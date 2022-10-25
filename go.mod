2533b48ae975488d110dcf90bb42b62c674431b3
Shuji <zaha@cprtnr.com>
2022-10-25 09:51:24 +0900

    UserCRUDの作成

--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.18
 
 require gopkg.in/go-ini/ini.v1 v1.67.0
 
+require (
+	github.com/google/uuid v1.3.0
+	github.com/mattn/go-sqlite3 v1.14.15
+)
+
 require github.com/stretchr/testify v1.8.0 // indirect
--- main.go
+++ main.go
@@ -2,13 +2,39 @@ package main
 
 import (
 	"fmt"
-
-	"golang_basic/Go_Todo_App/config"
+	"golang_basic/Go_Todo_App/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	/*
+		fmt.Println(config.Config.Port)
+		fmt.Println(config.Config.SQLDriver)
+		fmt.Println(config.Config.DbName)
+		fmt.Println(config.Config.LogFile)
+
+		log.Println("test")
+	*/
+	fmt.Println(models.Db)
+
+	/*
+		u := &models.User{}
+		u.Name = "Test"
+		u.Email = "test@exampl.com"
+		u.PassWord = "testtest"
+		fmt.Println(u)
+
+		u.CreateUser()
+	*/
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
+
+	u.Name = "Test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
+	u.DeleteUser()
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
 }
